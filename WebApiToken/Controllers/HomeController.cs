7473ae50d728aac7c102f24d6ccf3a9d9d4c6cfc
yasinguler35 <yasinguler007@gmail.com>
2021-05-16 18:09:39 +0300

    lohin sayfasıyla kullanıcı adı şifre girerek token oluşturuyorum jquery

---
+++ WebApiToken/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApiToken.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Login()
+        {
+            return View();
+        }
+    }
+}
